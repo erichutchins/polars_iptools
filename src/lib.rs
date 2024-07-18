2a5e0921e0ee9accb3e48dddf7d1e5fffacfa58f
Eric Hutchins <ehutchins@gmail.com>
2024-07-18 15:43:49 -0400

    properly expose __version__

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,7 @@ use pyo3::types::PyModule;
 use pyo3::{pymodule, Bound, PyResult};
 
 #[pymodule]
-#[pyo3(name = "_polars_iptools")]
-fn polars_iptools(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _internal(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
