4cd656e88104036eb3c22cd5bcb25040ecf7d29a
Eric Hutchins <ehutchins@gmail.com>
2024-08-15 19:16:17 -0400

    adapting to latest cookiecutter patterns

--- polars_iptools/__init__.py
+++ polars_iptools/__init__.py
@@ -1,3 +1,5 @@
+from polars_iptools._internal import __version__ as __version__
+
 import polars_iptools.geoip as geoip  # noqa: F401
 from polars_iptools._internal import __version__  # noqa: F401
 from polars_iptools.iptools import *  # noqa: F403
--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,15 @@ mod geoip;
 mod iptools;
 mod maxmind;
 mod utils;
-
-#[cfg(target_os = "linux")]
-use jemallocator::Jemalloc;
-
-#[global_allocator]
-#[cfg(target_os = "linux")]
-static ALLOC: Jemalloc = Jemalloc;
-
-use pyo3::types::PyModule;
+use pyo3::types::{PyModule, PyModuleMethods};
 use pyo3::{pymodule, Bound, PyResult};
+use pyo3_polars::PolarsAllocator;
 
 #[pymodule]
 fn _internal(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
+
+#[global_allocator]
+static ALLOC: PolarsAllocator = PolarsAllocator::new();
